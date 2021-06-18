5a279a5971c65671ceda8fc9f1ce67c4f8b4d068
GutemaG <bir13gud17@gmail.com>
2021-06-18 14:38:12 -0400

    creating bureau and vuex modules

---
+++ resources/js/components/Bureau.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="content">
+        <h1>Bureau</h1>
+    </div>
+</template>
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style scoped>
+.content {
+  margin-top: 1%;
+}
+</style>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -3,4 +3,5 @@ export default [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default },
     { path: '/profile', component: require('./components/Profile.vue').default },
     { path: '/users', component: require('./components/Users.vue').default },
+    { path: '/bureaus', component: require('./components/Bureau.vue').default },
 ];
