844c6451c3d1e04ebcd0cf5ac98eda9bb040ec2b
henokfekade <henokfekade2468@gmail.com>
2021-08-25 04:59:59 -1200

    online request can be updated with prerequisite

--- app/Http/Controllers/Actions/OnlineRequestPrerequisiteInputAction.php
+++ app/Http/Controllers/Actions/OnlineRequestPrerequisiteInputAction.php
@@ -21,11 +21,51 @@ class OnlineRequestPrerequisiteInputAction
                     $temp = [
                         'online_request_id' => $onlineRequestId,
                         'name' => $value['name'],
-                        'input_id' => $value['id'],
+                        'input_id' => $value['input_id'],
                         'type' => $value['type'],
                     ];
                     self::store($temp);
                 }
         }
+    }
+
+    public static function update(array $data): void
+    {
+        foreach ($data as $value) {
+            $procedure = OnlineRequestPrerequisiteInput::find($value['id']);
+            $procedure->update($value);
+        }
+    }
+
+    public static function updateData(array $data, int $onlineRequestId): void
+    {
+        if (array_key_exists('prerequisites', $data))
+            if (array_key_exists('inputs', $data['prerequisites'])) {
+                list($updatable, $creatable) = self::getUpdatableAndCreatableData($data['prerequisites']['inputs']);
+                self::storeData(['prerequisites'=>['inputs' => $creatable]], $onlineRequestId);
+                self::update($updatable);
+            }
+    }
+
+    /**
+     * Categorize each value of incoming data to updatable and creatable.
+     * So that the new procedure added will be stored and the existing procedure will be updated.
+     *
+     * @param $data
+     * @return array[]
+     */
+    protected static function getUpdatableAndCreatableData(array $data): array
+    {
+        $updatable = [];
+        $creatable = [];
+
+        collect($data)
+            ->filter(function ($value) use (&$updatable, &$creatable) {
+                array_key_exists('id', $value) ?
+                    $updatable[] = $value :
+                    $creatable[] = $value;
+
+            });
+        return array($updatable, $creatable);
     }
 }
--- app/Http/Controllers/Actions/OnlineRequestPrerequisiteNoteAction.php
+++ app/Http/Controllers/Actions/OnlineRequestPrerequisiteNoteAction.php
@@ -23,4 +23,44 @@ class OnlineRequestPrerequisiteNoteAction
                 }
         }
     }
+
+    public static function update(array $data): void
+    {
+        foreach ($data as $value) {
+            $procedure = OnlineRequestPrerequisiteNote::find($value['id']);
+            $procedure->update($value);
+        }
+    }
+
+    public static function updateData(array $data, int $onlineRequestId): void
+    {
+        if (array_key_exists('prerequisites', $data))
+            if (array_key_exists('notes', $data['prerequisites'])) {
+                list($updatable, $creatable) = self::getUpdatableAndCreatableData($data['prerequisites']['notes']);
+                self::storeData(['prerequisites'=>['notes' => $creatable]], $onlineRequestId);
+                self::update($updatable);
+            }
+    }
+
+    /**
+     * Categorize each value of incoming data to updatable and creatable.
+     * So that the new procedure added will be stored and the existing procedure will be updated.
+     *
+     * @param $data
+     * @return array[]
+     */
+    protected static function getUpdatableAndCreatableData(array $data): array
+    {
+        $updatable = [];
+        $creatable = [];
+
+        collect($data)
+            ->filter(function ($value) use (&$updatable, &$creatable) {
+                array_key_exists('id', $value) ?
+                    $updatable[] = $value :
+                    $creatable[] = $value['note'];
+
+            });
+        return array($updatable, $creatable);
+    }
 }
