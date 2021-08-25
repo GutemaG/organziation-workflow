f6fa523c365cb2f7eaf335fcd5ebca68ff70dcdc
Henzz <khenok53@gmail.com>
2021-08-26 00:32:37 +0300

    responsive faq, guestwelcome, guestnav

--- resources/js/components/welcome/Home.vue
+++ resources/js/components/welcome/Home.vue
@@ -1,146 +1,105 @@
 <template>
-    <div>
-        <b-row>
-            <b-col>
-                <b-container fluid>
-                    <div class="mb-3">
-                  
-                        <b-carousel
-                            id="carousel-fade"
-                            style="text-shadow: 0px 0px 2px #000"
-                            fade
-                            indicators
-                            img-width="1024"
-                            img-height="480"
-                        >
-                            <b-carousel-slide
-                            caption="First slide"
-                            img-src="/images/astu gate.jpg"
-                            ></b-carousel-slide>
-                            <b-carousel-slide
-                            caption="Second Slide"
-                            img-src="/images/astu gate.jpg"
-                            ></b-carousel-slide>
-                            <b-carousel-slide
-                            caption="Third Slide"
-                            img-src="/images/astu gate.jpg"
-                            ></b-carousel-slide>
-                        </b-carousel>
-                    </div>
-
-                    <div class="mt-5 mb-5">
-                      <h2 style="text-transform: uppercase;"><b>Adama Science And Technology University</b></h2>
-                      <hr>
-                      <p>
-                        <b>University Name::</b> Adama Science and Technology University
-                        <br>
-                        <b>World Rank:: </b>9075
-                        <br>
-                        <b>Telephone Number::</b> +251 (221) 100 003
-                        <br>
-                        <b>Address::</b> Adama 1888 Oromia Ethiopia
-                        <br>
-                        <b>University Website::</b> http://www.astu.edu.et
-                        <br>
-                        About Adama Science and Technology University:
-
-                        Established in 1993, Adama Science and Technology University is a non-profit public higher-education institution 
-                        located in the urban setting of the large city of Adama (population range of 500,000-1,000,000 inhabitants), Oromia. This 
-                        institution also has a branch campus in Asella. Officially recognized by the Ministry of Education of Ethiopia, 
-                        Adama Science and Technology University (ASTU) is a large (uniRank enrollment range: 20,000-24,999 students) coeducational 
-                        Ethiopian higher education institution. Adama Science and Technology University (ASTU) offers courses and programs leading 
-                        to officially recognized higher education degrees such as bachelor degrees, master degrees, doctorate degrees in several areas 
-                        of study. See the uniRank degree levels and areas of study matrix below for further details. This 27 years old Ethiopian 
-                        higher-education institution has a selective admission policy This higher education institution has an open admission policy. 
-                        ASTU also provides several academic and non-academic facilities and services to students including a library, housing, sports 
-                        facilities, financial aids and/or scholarships, study abroad and exchange programs, online courses and distance learning opportunities, 
-                        as well as administrative services.
-                      </p>
-                        <h3>
-                            <b>Office of Academic Affairs</b> 
-                        </h3>
-                        <hr>
-                        <p>
-
-                            Adama Science and Technology University, ASTU, is one of the two science and technology universities established to
-                            support the industrial development of the nation by producing competent and leading graduates in science and technology fields.
-                            The university is working aggressively to produce qualified, competent, and socially responsible professionals in the fields of science and
-                            technology through promoting research oriented science and technology education. The academic wing has taken the leading role to ensure
-                            quality education which helps to realize the goals and ultimately the mission of ASTU.  The academic affairs office was established under
-                            the Vice President for Academic Affairs (VPA) to lead all academic and academic-related matters of the university.
-                            Adama Science And Technology University Guidance System website
-                            will help guide you through with any affairs you have with our
-                            campus. To get any information related to your affair go click
-                            the info button and search any kind of affair you have. But if
-                            you want get your affair done online go and click the online
-                            button below.
-                        </p>
-
-                        <p>
-                            Adama Science and Technology University has signed a consultant
-                            contract agreement with Seoul National University. Accordingly, the consultant, Seoul National University, is
-                            expected to facilitate the design, supervision, and bid document preparation of the ASTU-EDCF project.
-                            It is recalled that the government of South Korea has agreed to finance the establishment of five Centers of Excellence in
-                            ASTU with an outlay of one hundred million US Dollars. The agreement was signed between the governments of FDRE the Republic of Korea.
-                            T June 24, 2021
-                        </p>
-
-                    </div>
-                </b-container>
-            </b-col>
-            
-        </b-row>
+  <div>
+    <b-container fluid>
+      <div>
+        <b-carousel
+          style="text-shadow: 0px 0px 2px #000"
+          fade
+          indicators
+          img-width="1024"
+          img-height="480">
+            <b-carousel-slide
+            caption="First slide"
+            img-src="/images/astu gate.jpg"
+            ></b-carousel-slide>
+            <b-carousel-slide
+            caption="Second Slide"
+            img-src="/images/astu gate.jpg"
+            ></b-carousel-slide>
+            <b-carousel-slide
+            caption="Third Slide"
+            img-src="/images/astu gate.jpg"
+            ></b-carousel-slide>
+        </b-carousel>
       </div>
+
+      <div class="mt-5 mb-5">
+        <h2 style="text-transform: uppercase;"><b>Adama Science And Technology University</b></h2>
+        <hr>
+        <p>
+          <b>University Name::</b> Adama Science and Technology University
+          <br>
+          <b>World Rank:: </b>9075
+          <br>
+          <b>Telephone Number::</b> +251 (221) 100 003
+          <br>
+          <b>Address::</b> Adama 1888 Oromia Ethiopia
+          <br>
+          <b>University Website::</b> http://www.astu.edu.et
+          <br>
+          About Adama Science and Technology University:
+
+          Established in 1993, Adama Science and Technology University is a non-profit public higher-education institution 
+          located in the urban setting of the large city of Adama (population range of 500,000-1,000,000 inhabitants), Oromia. This 
+          institution also has a branch campus in Asella. Officially recognized by the Ministry of Education of Ethiopia, 
+          Adama Science and Technology University (ASTU) is a large (uniRank enrollment range: 20,000-24,999 students) coeducational 
+          Ethiopian higher education institution. Adama Science and Technology University (ASTU) offers courses and programs leading 
+          to officially recognized higher education degrees such as bachelor degrees, master degrees, doctorate degrees in several areas 
+          of study. See the uniRank degree levels and areas of study matrix below for further details. This 27 years old Ethiopian 
+          higher-education institution has a selective admission policy This higher education institution has an open admission policy. 
+          ASTU also provides several academic and non-academic facilities and services to students including a library, housing, sports 
+          facilities, financial aids and/or scholarships, study abroad and exchange programs, online courses and distance learning opportunities, 
+          as well as administrative services.
+        </p>
+          <h3>
+              <b>Office of Academic Affairs</b> 
+          </h3>
+          <hr>
+          <p>
+            Adama Science and Technology University, ASTU, is one of the two science and technology universities established to
+            support the industrial development of the nation by producing competent and leading graduates in science and technology fields.
+            The university is working aggressively to produce qualified, competent, and socially responsible professionals in the fields of science and
+            technology through promoting research oriented science and technology education. The academic wing has taken the leading role to ensure
+            quality education which helps to realize the goals and ultimately the mission of ASTU.  The academic affairs office was established under
+            the Vice President for Academic Affairs (VPA) to lead all academic and academic-related matters of the university.
+            Adama Science And Technology University Guidance System website
+            will help guide you through with any affairs you have with our
+            campus. To get any information related to your affair go click
+            the info button and search any kind of affair you have. But if
+            you want get your affair done online go and click the online
+            button below.
+          </p>
+
+          <p>
+            Adama Science and Technology University has signed a consultant
+            contract agreement with Seoul National University. Accordingly, the consultant, Seoul National University, is
+            expected to facilitate the design, supervision, and bid document preparation of the ASTU-EDCF project.
+            It is recalled that the government of South Korea has agreed to finance the establishment of five Centers of Excellence in
+            ASTU with an outlay of one hundred million US Dollars. The agreement was signed between the governments of FDRE the Republic of Korea.
+            T June 24, 2021
+          </p>
+      </div>
+    </b-container>
+  </div>
 </template>
 
 <script>
+
 export default {
     data() {
       return {
-        slide: 0,
-        sliding: null,
-        affairs:[]
+        
       }
-    },
-    computed:{
-      first() {
-        return this.affairs[0];
-      },
     },
     methods: {
-      onSlideStart(slide) {
-        this.sliding = true
-      },
-      onSlideEnd(slide) {
-        this.sliding = false
-      },
-      getAffairs(){
-        console.log('created')
-      }
-    },
-    created() {
-      // this.fetchAffairs()
-      // this.$store.dispatch('fetchAffairs')
-      this.getAffairs();
-    },
+      
+    }
 };
+
 </script>
 
 <style scoped>
-*{
+* {
   line-height: 2.14rem;
-}
-.contain {
-  max-width: 960px;
-  width: 100%;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
-  box-sizing: border-box;
-}
-
-.bLink i{
-    color: rgb(255, 255, 255);
 }
 </style>
